2baad965650b4c6cba7c260ae3f197451af87bb8
coudrew <AMARNA@Andrews-Mini.home>
2016-02-27 13:57:28 +0000

    first commit

---
+++ Source/SKGameData.h
@@ -0,0 +1,13 @@
+//
+//  SKGameData.h
+//  Scramble Kittty!
+//
+//  Created by Andrew R Couture on 2015-07-29.
+//  Copyright (c) 2015 coudrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKGameData : NSObject
+
+@end
---
+++ Source/SKGameData.m
@@ -0,0 +1,13 @@
+//
+//  SKGameData.m
+//  Scramble Kittty!
+//
+//  Created by Andrew R Couture on 2015-07-29.
+//  Copyright (c) 2015 coudrew. All rights reserved.
+//
+
+#import "SKGameData.h"
+
+@implementation SKGameData
+
+@end
